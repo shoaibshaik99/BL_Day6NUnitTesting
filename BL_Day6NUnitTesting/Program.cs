7a0b960260861b2f5b41bab4799e65b4a054a9a4
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-24 00:21:26 +0530

    [Shoaib] First sync of local and remote main

---
+++ BL_Day6NUnitTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BL_Day6NUnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
