d114f603ae029a20b5f89f51cf2860074b92c017
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-24 23:21:25 +0530

    [Shoaib] Adding all the N unit testing problems except the sqrt function which runs using Newton's Method

---
+++ BL_Day6NUnitTesting/Binary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_Day6NUnitTesting
+{
+    internal class Binary
+    {
+        public static void SwapNibbles()
+        {
+            /*Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());*/
+            string num = Util.NumberToBinary();
+            String tempNum = "";
+
+            //Assigning second nibble
+            for (int i = 4;i <8;i++)
+            {
+                tempNum = tempNum + num[i];
+            }
+
+            //Assigning first nibble
+            for (int i = 0; i < 4; i++)
+            {
+                tempNum = tempNum + num[i];
+            }
+            int nibbleSwappedNum = 0;
+            for (int i = 0; i < tempNum.Length; i++)
+            {
+                if (tempNum[i] == '1')
+                {
+                    nibbleSwappedNum += (int)Math.Pow(2, tempNum.Length - i - 1);
+                }
+            }
+
+            Console.WriteLine("Number after swapping nibbles is: {0}", nibbleSwappedNum);
+
+            //return nibbleSwappedNum;
+        }
+    }
+}
---
+++ BL_Day6NUnitTesting/Util.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace BL_Day6NUnitTesting
+{
+    internal class Util
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a program number to run it:\n1.Fewest notes for given amount of currency.\n2.Day of the week" +
+                "\n3.Temperature conversion\n4.Monthly payment on the borrowed amount\n5.Numebr to Binary\n6.Swap nibbles in Binary");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Util.FewestNotes();
+                    break;
+                case 2:
+                    {
+                        int d, m, y;
+                        Console.WriteLine("Enter the day in the date:");
+                        d = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the month in the date:");
+                        m = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the year in the date:");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Day of the week: {0}", Util.DayOfTheWeek(d, m, y));
+                    };
+                    break;
+                case 3:
+                    Util.TemperatureConversion();
+                    break;
+                case 4:
+                    {
+                        Console.Write("Enter the amount of principal: ");
+                        double p = Convert.ToDouble(Console.ReadLine());
+                        // What exactly is the difference between Double.Parse and Convert.ToDouble
+
+                        Console.Write("Enter the number of years for repayment: ");
+                        double y = Convert.ToDouble(Console.ReadLine());
+
+                        Console.Write("Enter the rate of interest: ");
+                        double r = Convert.ToDouble(Console.ReadLine());
+
+                        Util.MonthlyPayment(p,y,r);
+                    };
+                    break;
+                case 5:
+                    {
+                        string binary = Util.NumberToBinary();
+
+                        Console.WriteLine("Binary representation of the number is {0}", binary);
+                    }
+                    break;
+                case 6:
+                    Util.SwapNibbles();
+                    break;
+                default:
+                    Console.WriteLine("Incorrect input, program ends here");
+                    break;
+            }
+        }
+        public static void FewestNotes()
+
+        {
+            Console.WriteLine("Enter the total amount of currency: ");
+            int totalAmt = Convert.ToInt32(Console.ReadLine()); // Total amount
+            int amt = totalAmt;
+            int totalNoOfNotes = 0;
+            int[] currencyDenominatios = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            Console.WriteLine("Currency denominations available are : ");
+            foreach (int denomination in currencyDenominatios)
+            {
+                Console.Write(denomination + " ");
+            }
+            Console.WriteLine();
+            int[] noOfNotes = new int[currencyDenominatios.Length];
+            for (int i = 0; i < currencyDenominatios.Length; i++)
+            {
+                noOfNotes[i] = amt / (currencyDenominatios[i]);
+                amt = amt % (currencyDenominatios[i]);
+            }
+            Console.WriteLine("To disburse minimum number of notes:");
+            for (int i = 0; i < currencyDenominatios.Length; i++)
+            {
+                Console.WriteLine("Number of {0} Rupee notes required are {1}", currencyDenominatios[i], noOfNotes[i]);
+                totalNoOfNotes += noOfNotes[i];
+            }
+            Console.WriteLine("Total number of notes disbursed are {0}", totalNoOfNotes);
+        }
+
+        public static int DayOfTheWeek( int d, int m, int y)
+        {
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
+            return d0;
+        }
+
+        public static void TemperatureConversion()
+        {
+            Console.WriteLine("Chose a type of conversion:\n1.Celsius to Fahrenheit:\n2.Fahrenheit to Celsius:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    {
+                        double c, f; //Celsius & Farenheit
+                        Console.WriteLine("Enter the temperature in Celsius (output will be in Farenheit): ");
+                        c = Convert.ToDouble(Console.ReadLine());
+                        f = 32 + 1.8 * c;
+                        Console.WriteLine("Temperature in Farenheit is {0}", f);
+                    };
+                    break;
+                case 2:
+                    {
+                        double c, f; //Celsius & Farenheit
+                        Console.WriteLine("Enter the temperature in Farenheit (output will be in Celsius): ");
+                        f = Convert.ToDouble(Console.ReadLine());
+                        c = (f-32)*(5.0/9.0);
+                        Console.WriteLine("Temperature in celsius is {0}", c);
+                    };
+                    break;
+                default:
+                    Console.WriteLine("Incorrect Input, program ends here");
+                    break;
+            }
+        }
+        public static void MonthlyPayment(double p, double y, double r)
+        {
+            double noOfMonths = 12 * y;
+            double ratePerMonth = r / (12 * 100);
+            double monthlyPayment = (p * ratePerMonth) / (1 - (Math.Pow(1 + ratePerMonth, -noOfMonths))) ;
+            Console.WriteLine("Monthly payment: {0}", monthlyPayment);
+
+        }
+
+        public static string NumberToBinary()
+        {
+            Console.WriteLine("Enter a number to see it's binary representation: ");
+            int num = Convert.ToInt32 (Console.ReadLine());
+            int tempNum = num;
+            /*if (num < 0)
+            {
+                tempNum = -num;
+            }
+            string binary = "";
+            while (tempNum != 0)
+            {
+                binary = tempNum%2 + binary;
+                tempNum = tempNum/2;
+            }
+            if (num < 0)
+            {
+                binary = "-" + binary;
+            }
+            Console.WriteLine("Binary representation of the number {0} is {1}", num, binary);
+            return binary;*/
+
+            /*if (num == 0)
+            {
+                return "0";
+            }
+
+            string binary = string.Empty;
+
+            while (tempNum > 0)
+            {
+                int remainder = tempNum % 2;
+                binary = remainder + binary;
+                tempNum /= 2;
+            }*/
+            string binary = Convert.ToString(num, 2).PadLeft(8, '0');
+
+            return binary;
+        }
+
+            public static void SwapNibbles()
+            {
+                Binary.SwapNibbles();
+            }
+    }
+}
